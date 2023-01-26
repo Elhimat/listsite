aa16751ddb7246c4663fbee1ae92a72b35730e1c
AzureAD\EliasMattsson <46132@gymnasium.ax>
2023-01-26 10:20:38 +0200

    database

--- listsite/settings.py
+++ listsite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'listsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'rJ2Oucev61bRVaFdWeck',
+        'HOST': 'containers-us-west-197.railway.app',
+        'PORT': '5518',
     }
 }
 
